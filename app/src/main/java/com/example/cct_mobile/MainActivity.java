f8a6558d2b20da67c8b9c5968b5acfa5ef5eb3d5
Lohith <lohith.srt@gmail.com>
2022-02-22 19:53:47 -0400

    login and sign up

--- app/src/main/java/com/example/cct_mobile/MainActivity.java
+++ app/src/main/java/com/example/cct_mobile/MainActivity.java
@@ -2,13 +2,60 @@ package com.example.cct_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button loginButton;
+    EditText phoneNumberEditText;
+    String phoneNumber;
+    TextView redirectToSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.loginButton);
+        phoneNumberEditText = findViewById(R.id.phoneNumberEditTextLogin);
+        redirectToSignUp = findViewById(R.id.singUpTextView);
+
+        redirectToSignUp.setOnClickListener(this);
+        loginButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v)
+    {
+        // Depending on what button is pressed
+        switch(v.getId()){
+            case R.id.loginButton:
+                if (isPhoneNumberValid()){
+                    startActivity(new Intent(this, signup.class));
+                } else {
+                    Toast.makeText(this, "Invalid Phone Number", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.singUpTextView:
+                startActivity(new Intent(this, signup.class));
+                break;
+        }
+    }
+
+    private boolean isPhoneNumberValid(){
+        phoneNumber = phoneNumberEditText.getText().toString();
+        if(phoneNumber.charAt(0) != '+') return false;
+        if(phoneNumber.length()<11)  return false;
+        return true;
+    }
+
+
+
+
 }
--- app/src/main/java/com/example/cct_mobile/signup.java
+++ app/src/main/java/com/example/cct_mobile/signup.java
@@ -2,13 +2,59 @@ package com.example.cct_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class signup extends AppCompatActivity {
+public class signup extends AppCompatActivity implements View.OnClickListener{
+
+    Button signupButton;
+    EditText phoneNumberEditText;
+    String phoneNumber;
+    TextView redirectToLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+
+        signupButton = findViewById(R.id.signupButton);
+        phoneNumberEditText = findViewById(R.id.phoneNumberEditTextSignUp);
+        redirectToLogin = findViewById(R.id.redirectToLoginTextView);
+
+        redirectToLogin.setOnClickListener(this);
+        signupButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.signupButton:
+                phoneNumber = phoneNumberEditText.getText().toString();
+                if(isPhoneNumberValid()){
+                    signUpUser();
+                    startActivity(new Intent(this, MainActivity.class));
+                } else {
+                    Toast.makeText(this, "Invalid Phone number", Toast.LENGTH_SHORT).show();
+                }
+            case R.id.redirectToLoginTextView:
+                startActivity(new Intent(this, MainActivity.class));
+
+        }
+    }
+
+    private boolean isPhoneNumberValid(){
+        if(phoneNumber.charAt(0) != '+') return false;
+        if(phoneNumber.length()<11)  return false;
+        return true;
+    }
+
+    public void signUpUser(){
+
     }
 }
